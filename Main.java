286ddd9af9cb6bdc627c68fceb847a6c09870cd8
JOSE-VARGAS <josevb@ciencias.unam.mx>
2015-11-10 20:26:15 -0600

    Subiendo clases

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+import java.awt.*;
+import javax.swing.*;
+import java.applet.*;
+
+public class Main {
+
+	public static void main(String [] args) {
+		
+		
+		/*
+		BufferedImage img = FileIO.readImage("data/ml.bmp");
+		Graphics2D g = img.createGraphics();		
+
+        g.setColor(Color.white);
+        g.fillRect(100, 100, 400, 400);
+        g.drawImage(img, 0, 0, null);
+        JFrame jp1 = new JFrame();
+        So1 a=new So1 ();
+        Lienzo x = new Lienzo();
+        //jp1.getContentPane().add(a, BorderLayout.CENTER);
+        jp1.getContentPane().add(g, BorderLayout.CENTER);
+        jp1.setSize(new Dimension(500,500));
+        jp1.setVisible(true);
+		*/
+		Surface c = new Surface();
+    
+	}
+}
---
+++ So1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+import java.applet.*;
+
+public class So1 extends Applet{
+
+    public void paint (Graphics g)
+    {
+        g.drawString("hello",40,30);
+    }
+
+}
